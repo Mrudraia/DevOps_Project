d2d981c79a7b5110a74110f70e970624bc63a202
mrudraia <mrudraia@redhat.com>
2024-05-04 15:31:49 +0530

    sixth commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "server" {
     ami = "ami-0e001c9271cf7f3b9"
     instance_type = "t2.micro"
     key_name = aws_key_pair.deployer.key_name
-    vpc_security_group_ids = aws_security_group.maingroup.vpc_security_group_ids
+    vpc_security_group_ids = [ aws_security_group.maingroup.id ]
     iam_instance_profile = aws_iam_instance_profile.ec2-profile.name
     connection {
       host = self.public_ip
